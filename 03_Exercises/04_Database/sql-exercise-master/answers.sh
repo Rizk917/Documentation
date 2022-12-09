694af9be31ec9d3a1f1b652c9d3e2f7f9c78f7f0
Rizk917 <117639834+Rizk917@users.noreply.github.com>
2022-12-09 14:51:42 +0200

    Basic Queries

---
+++ 03_Exercises/04_Database/sql-exercise-master/answers.sh
@@ -0,0 +1,7 @@
+select name FROM students 
+select name FROM students WHERE Age >30
+select name FROM students WHERE Age >30 AND Gender="F"
+select Points FROM students WHERE name="Alex"
+INSERT INTO students VALUES ('7','Mhmd','25','M','700');
+UPDATE students  SET Points="700" WHERE name="BASMA"
+UPDATE students SET Points="150" WHERE name="Alex"
