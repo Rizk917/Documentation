b9d903acdc993b484b802f1cbd0a2ff469444227
Rizk917 <117639834+Rizk917@users.noreply.github.com>
2022-12-10 16:53:14 +0200

    Creating Table

--- 03_Exercises/04_Database/sql-exercise-master/answers.sh
+++ 03_Exercises/04_Database/sql-exercise-master/answers.sh
@@ -3,5 +3,27 @@ select name FROM students WHERE Age >30
 select name FROM students WHERE Age >30 AND Gender="F"
 select Points FROM students WHERE name="Alex"
 INSERT INTO students VALUES ('7','Mhmd','25','M','700');
-UPDATE students  SET Points="700" WHERE name="BASMA"
+UPDATE students  SET Points="700" WHERE name="Basma"
 UPDATE students SET Points="150" WHERE name="Alex"
+______________________________________
+
+CREATE TABLE "graduates" (
+	"ID"	INTEGER NOT NULL,
+	"Name"	TEXT NOT NULL UNIQUE,
+	"Age"	INTEGER,
+	"Gender"	TEXT,
+	"Points"	INTEGER,
+	"Graduation"	TEXT,
+	PRIMARY KEY("ID" AUTOINCREMENT)
+);
+______________________________________
+INSERT INTO graduates (ID, name,Age, Gender, Points)
+SELECT ID, name,Age, Gender, Points
+FROM students
+WHERE name="Basma";
+______________________________________
+UPDATE graduates  SET Graduation="25/07/2022" WHERE name="Basma"
+______________________________________
+DELETE from students WHERE name="Layal"
+_______________________________________
+______________________________________
