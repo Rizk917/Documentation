3178910b667f0e54cd5e79db5bc95ecf1c87a029
Rizk917 <117639834+Rizk917@users.noreply.github.com>
2023-01-03 21:12:10 +0200

    step 3

--- 04_Challenges/movie-database/index.js
+++ 04_Challenges/movie-database/index.js
@@ -1,11 +1,27 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const tResponse={ status:200, message:"ok"};
+let date_ob = new Date();
 
 app.get('/', (req, res) => {
   res.send('OK')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.get('/test', (req, res) => {
+    res.send(tResponse)
+  })
+//   app.listen(port, () => {
+//     console.log(`ok`)
+//   })
+  app.get('/time', (req, res) => {
+    // current hours
+let hours = date_ob.getHours();
+
+// current minutes
+let minutes = date_ob.getMinutes();
+if(minutes<10){minutes='0'+minutes}
+
+const timeResponse={status:200, message:hours+':'+minutes};
+    res.send(timeResponse)
+  })
